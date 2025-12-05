3b6c21d3f4a3497d12fc0750504c947b8652b266
r0masaN <romeopro150703@gmail.com>
2025-12-05 06:18:29 +0300

    refactored pretty_view.hpp (friend operator<< is extracted from the struct)

--- pretty_view/pretty_view.hpp
+++ pretty_view/pretty_view.hpp
@@ -36,56 +36,72 @@ namespace rmsn {
         std::tuple_size<base_t<T>>::value;
     };
 
+    // declaration of the struct (to be visible in the following operator<<)
+    template<typename T>
+    requires is_collection<T> || is_tuple<T>
+    struct pretty_view;
+
+    // declaration of the operator<<
+    template<typename T>
+    requires is_collection<T> || is_tuple<T>
+    std::ostream& operator<<(std::ostream& os, const pretty_view<T>& pv) noexcept;
+
     // simple proxy class for collections and tuples (to prevent ADL from breaking overloadings or messing with some shit in std)
     template<typename T>
-    requires (is_collection<T> || is_tuple<T>)
+    requires is_collection<T> || is_tuple<T>
     struct pretty_view {
         explicit pretty_view(const T& t) noexcept : _t(t) {}
 
-        friend std::ostream& operator<<(std::ostream& os, const pretty_view<T>& pv) noexcept {
-            if constexpr (is_collection<T>) { // if proxy contains collection
-                os << pretty_view_helper::collection_prefix;
-
-                const auto begin = std::begin(pv._t), end = std::end(pv._t);
-                using elem_t = base_t<decltype(*begin)>; // dirty type (I don't care cuz it will be cleaned in concepts)
-
-                for (auto it = begin; it != end; ++it) { // iterating on collection
-                    if (it != begin) os << pretty_view_helper::collection_delimiter;
-                    // if collection's element is collection or tuple himself
-                    if constexpr (is_collection<elem_t> || is_tuple<elem_t>) os << pretty_view<elem_t>(*it); // wrap in proxy => recursion
-                    else os << *it; // else it's primitive or class/struct that isn't collection/tuple
-                }
-
-                os << pretty_view_helper::collection_postfix;
-
-            } else if constexpr (is_tuple<T>) { // if proxy contains tuple
-                os << pretty_view_helper::tuple_prefix;
-
-                // fun :) it's anonymous lambda that's unwrapping index sequence made from tuple
-                [&]<std::size_t... I>(std::index_sequence<I...>) {
-                    ( // 67-77 lines will be applied for each unwrapped element from tuple
-                            ( // if that's the first element of tuple, don't write delimiter (before him)
-                                    I == 0 ? void() : void(os << pretty_view_helper::tuple_delimiter),
-                                    [&]() { // another anonymous lambda that does the same logic that 52-57 lines
-                                        const auto& elem = std::get<I>(pv._t); // std::get<I>(pv._t) gets an I-st element from tuple pv.t
-                                        using elem_t = decltype(elem);
-                                        if constexpr (is_collection<elem_t> || is_tuple<elem_t>) os << pretty_view<elem_t>(elem);
-                                        else os << elem;
-                                    }() // immediately invoke this lambda
-                            ),
-                            ... // unwrapping variadic pack
-                    );
-                }(std::make_index_sequence<std::tuple_size_v<base_t<T>>>{}); // here comes an index sequence + immediately invocation
-
-                os << pretty_view_helper::tuple_postfix;
-            } // there are no other if-else branches cuz we work here only with collections and tuples
-
-            return os;
-        }
+        // declaration that that operator<< is friend (has access to private fields)
+        friend std::ostream& operator<<(std::ostream& os, const pretty_view<T>& pv) noexcept;
 
     private:
         const T& _t;
     };
+
+    // realization of the operator<<
+    template<typename T>
+    requires is_collection<T> || is_tuple<T>
+    std::ostream& operator<<(std::ostream& os, const pretty_view<T>& pv) noexcept {
+        if constexpr (is_collection<T>) { // if proxy contains collection
+            os << pretty_view_helper::collection_prefix;
+
+            const auto begin = std::begin(pv._t), end = std::end(pv._t);
+            using elem_t = base_t<decltype(*begin)>; // dirty type (I don't care cuz it will be cleaned in concepts)
+
+            for (auto it = begin; it != end; ++it) { // iterating on collection
+                if (it != begin) os << pretty_view_helper::collection_delimiter;
+                // if collection's element is collection or tuple himself
+                if constexpr (is_collection<elem_t> || is_tuple<elem_t>) os << pretty_view<elem_t>(*it); // wrap in proxy => recursion
+                else os << *it; // else it's primitive or class/struct that isn't collection/tuple
+            }
+
+            os << pretty_view_helper::collection_postfix;
+
+        } else if constexpr (is_tuple<T>) { // if proxy contains tuple
+            os << pretty_view_helper::tuple_prefix;
+
+            // fun :) it's anonymous lambda that's unwrapping index sequence made from tuple
+            [&]<std::size_t... I>(std::index_sequence<I...>) {
+                ( // 67-77 lines will be applied for each unwrapped element from tuple
+                        ( // if that's the first element of tuple, don't write delimiter (before him)
+                                I == 0 ? void() : void(os << pretty_view_helper::tuple_delimiter),
+                                        [&]() { // another anonymous lambda that does the same logic that 52-57 lines
+                                            const auto& elem = std::get<I>(pv._t); // std::get<I>(pv._t) gets an I-st element from tuple pv.t
+                                            using elem_t = decltype(elem);
+                                            if constexpr (is_collection<elem_t> || is_tuple<elem_t>) os << pretty_view<elem_t>(elem);
+                                            else os << elem;
+                                        }() // immediately invoke this lambda
+                        ),
+                        ... // unwrapping variadic pack
+                );
+            }(std::make_index_sequence<std::tuple_size_v<base_t<T>>>{}); // here comes an index sequence + immediately invocation
+
+            os << pretty_view_helper::tuple_postfix;
+        } // there are no other if-else branches cuz we work here only with collections and tuples
+
+        return os;
+    }
 }
 
 #endif
